c21a595d841b1229283d45d4484586ac0509871d
Justin Alec M. Antonio <justinantonio787@yahoo.com>
2024-11-15 08:48:41 +0800

    first integration

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    fetch('/')
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+export default App;     
